632c38ec26491ab19178b69ad13bc3655b324684
Bhavesh Garg <bhavesh0336.cse19@chitkara.edu.in>
2021-06-20 00:49:36 +0530

    Updated File

--- WebScrapingProject3/main.js
+++ WebScrapingProject3/main.js
@@ -1,8 +1,13 @@
 const request = require('request');
 const cheerio = require('cheerio');
+const fs = require('fs');
+const path = require('path');
 const getAllDetailsOfTheMatch = require('./allMatch');
 
 const url = "https://www.espncricinfo.com/series/ipl-2020-21-1210595/";
+const iplPath = path.join(__dirname, "IPL");
+
+createDirectories(iplPath);
 
 console.log("Before Data Fetching!😀");
 request(url, callback);
@@ -21,4 +26,10 @@ function handleHTML(html) {
     let viewResultButton = $(".widget-items.cta-link a").attr("href");
     let urlToNewPage = "https://www.espncricinfo.com/" + viewResultButton;
     getAllDetailsOfTheMatch.getAllMatchesDetails(urlToNewPage);
+}
+
+function createDirectories(filePath) {
+    if (fs.existsSync(filePath) == false) {
+        fs.mkdirSync(filePath);
+    }
 }
