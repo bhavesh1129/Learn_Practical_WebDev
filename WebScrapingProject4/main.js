cb6686977102ef7e56143370fb037cfe500afe4a
Bhavesh Garg <bhavesh0336.cse19@chitkara.edu.in>
2021-06-21 03:19:02 +0530

    Add Web Scraping Assignment Project

---
+++ WebScrapingProject4/main.js
@@ -0,0 +1,131 @@
+const request = require('request');
+const cheerio = require('cheerio');
+const path = require('path');
+const fs = require('fs');
+
+console.log("Before Data Fetching!😀");
+const url = "https://github.com/topics";
+
+request(url, callBack);
+console.log("After Data Fetching!😀");
+console.log("-------------------------------------------");
+
+function callBack(err, response, html) {
+    if (err) {
+        console.log("Opps its an error!😥 ", err);
+    } else {
+        linksOfThreeCards(html);
+    }
+}
+
+function linksOfThreeCards(html) {
+    let $ = cheerio.load(html);
+    let cardsURL = $(".topic-box a");
+    for (let i = 0; i < cardsURL.length; i++) {
+        let linkInCards = $(cardsURL[i]).attr("href");
+
+        //Making the Association's name dir
+        let nameOfAssociationOnly = linkInCards.split("/");
+        var nameOfDir = nameOfAssociationOnly[2];
+        if (!fs.existsSync(nameOfDir)) {
+            let dirPathh = path.join(__dirname, nameOfDir);
+            fs.mkdirSync(dirPathh);
+        }
+
+        let mainURL = "https://github.com";
+        let fullLinkInsideCards = mainURL + linkInCards;
+        console.log(fullLinkInsideCards);
+
+        findReposLink(fullLinkInsideCards, mainURL, nameOfDir);
+    }
+    console.log("-------------------------------------------");
+    console.log("-------------------------------------------");
+}
+
+function findReposLink(fullLinkInsideCards, mainURL, nameOfDir) {
+    request(fullLinkInsideCards, function(err, response, html) {
+        if (err) {
+            console.log("Opps its an error!😥 ", err);
+        } else {
+            extractRepoLinks(html, fullLinkInsideCards, mainURL, nameOfDir);
+        }
+    });
+}
+
+function extractRepoLinks(html, fullLinkInsideCards, mainURL, nameOfDir) {
+    let $ = cheerio.load(html);
+    let repoList = $(".px-3 .d-flex.flex-auto a");
+    for (let i = 0; i < 16; i++) {
+        if (i % 2 != 0) {
+            var getRepoLinks = $(repoList[i]).attr("href");
+            let fullRepoLink = fullLinkInsideCards + getRepoLinks;
+            console.log(fullRepoLink + "❌");
+
+            let mainRepoLink = mainURL + getRepoLinks;
+            console.log(mainRepoLink + "✔️");
+
+            getIssuesLinksFromRepo(mainRepoLink, nameOfDir);
+        }
+    }
+    console.log("-------------------------------------------");
+    console.log("-------------------------------------------");
+}
+
+function getIssuesLinksFromRepo(mainRepoLink, nameOfDir) {
+    request(mainRepoLink, function(err, response, html) {
+        if (err) {
+            console.log("Opps its an error!😥 ", err);
+        } else {
+            getIssueFromTheirRepo(html, nameOfDir);
+        }
+    });
+}
+
+function getIssueFromTheirRepo(html, nameOfDir) {
+    let $ = cheerio.load(html);
+    let issueLinks = $(".js-repo-nav  a");
+    let linksOfIssue = $(issueLinks[1]).attr("href");
+    if (linksOfIssue.includes("/issues")) {
+        console.log("-------------------------------------------");
+        let fullIssueLinks = "https://github.com" + linksOfIssue;
+        console.log(fullIssueLinks);
+
+        //Making the Repo's name file
+        let nameOfRepoForDir = linksOfIssue.split("/");
+        let repoNameForDir = nameOfRepoForDir[2];
+
+        let pathForRepo = path.join(__dirname, nameOfDir, repoNameForDir);
+        fs.writeFileSync(pathForRepo + ".json", "");
+
+        console.log(repoNameForDir);
+        someIssueMentioned(fullIssueLinks, pathForRepo);
+    }
+}
+
+function someIssueMentioned(fullIssueLinks, pathForRepo) {
+    request(fullIssueLinks, function(err, response, html) {
+        if (err) {
+            console.log("Opps its an error!😥 ", err);
+        } else {
+            listOfIssues(html, pathForRepo);
+        }
+    });
+}
+
+function listOfIssues(html, pathForRepo) {
+    let $ = cheerio.load(html);
+    let issuesDataInRepo = $(".flex-auto.min-width-0.p-2.pr-3.pr-md-2 a.Link--primary");
+    for (let i = 0; i < 10; i++) {
+        let issuesInRepo = $(issuesDataInRepo[i]).attr("href");
+        console.log(issuesInRepo);
+
+        //add issues to json file
+        let issuesObj = [
+            issuesInRepo,
+        ]
+        let issuesToJSONFile = JSON.stringify(issuesObj);
+        fs.writeFileSync(pathForRepo + ".json", issuesToJSONFile);
+    }
+    console.log("-------------------ISSUES NAME/ID-------------------");
+    console.log("----------------------------------------------------");
+}
